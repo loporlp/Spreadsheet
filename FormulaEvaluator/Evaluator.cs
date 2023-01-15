f80ec098597a58ebba9cdb6d5f591b05751492ba
Mason Sansom <masonsansom02@gmail.com>
2023-01-15 14:21:42 -0700

    Created Evaluator class and put in XML comments

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,25 @@
+﻿namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Author: Mason Sansom
+    /// Partner: -none-
+    /// Date: 15-Jan-2023
+    /// Course: CS 3500, University of Utah, School of Computing
+    /// Copyright: CS 3500 and Mason Sansom - This work may not be copied
+    /// 
+    /// I, Mason Sansom, certify that I wrote this code from scratch and did not use
+    /// another source
+    /// 
+    /// File Contents
+    /// 
+    /// 
+    /// </summary>
+    public class Evaluator
+    {
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+            // TODO...
+            throw new NotImplementedException();
+        }
+    }
+}
