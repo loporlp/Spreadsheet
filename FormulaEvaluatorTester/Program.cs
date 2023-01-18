01d048fad19f8c071f0319993bda59bb225434c8
Mason Sansom <masonsansom02@gmail.com>
2023-01-17 22:22:06 -0700

    Created partial tests for Evaluate

--- FormulaEvaluatorTester/Program.cs
+++ FormulaEvaluatorTester/Program.cs
@@ -2,6 +2,10 @@
 using FormulaEvaluator;
 using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine(Regex.IsMatch("A33", @"^\w+[0-9]+$"));
+Console.WriteLine(Evaluator.Evaluate("1", null) == 1);
+Console.WriteLine(Evaluator.Evaluate("2*8", null) == 16);
+Console.WriteLine(Evaluator.Evaluate("10+8", null) == 18);
+Console.WriteLine(Evaluator.Evaluate("8/4", null) == 2);
+Console.WriteLine(Evaluator.Evaluate("4 + 8/4", null) == 6);
+Console.WriteLine(Evaluator.Evaluate("2*     8", null) == 16);
+Console.WriteLine(Evaluator.Evaluate("2*8+16/4+18", null) == 38);
