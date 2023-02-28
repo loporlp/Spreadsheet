af58c9c6f029bd013a05daef3f1726f20f9e97dc
Mason Sansom <masonsansom02@gmail.com>
2023-02-28 16:08:44 -0700

    Spreadsheet now generates with entry boxes generated the correct way

--- GUI/MainPage.xaml.cs
+++ GUI/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace GUI
+﻿using System.Diagnostics;
+
+namespace GUI
 {
     public partial class MainPage : ContentPage
     {
@@ -16,7 +18,7 @@
             InitializeComponent();
 
             //Loop to add Top Labels
-            char[] letters = " ABCDEFGHIJKLMNOPQRSTUVWXWY".ToArray();
+            char[] letters = " ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToArray();
             foreach (char c in letters)
             {
                 TopLabels.Add(
@@ -60,8 +62,7 @@
                 );
             }
 
-            Dictionary<string, Entry> cells = new Dictionary<string, Entry>();
-            int num = 1;
+            Dictionary<string, MyEntry> cells = new Dictionary<string, MyEntry>();
             letters = letters[1..];
             //Loops to add Entry Boxes
             for (int i = 1; i < 10; i++)
@@ -71,6 +72,8 @@
 
                 foreach (char c in letters)
                 {
+                    MyEntry entry = new MyEntry(c, i, handleCellChanged);
+                    cells.Add(c + "" + i, entry);
                     horiz.Add(
                     new Border
                     {
@@ -79,24 +82,29 @@
                         HeightRequest = 30,
                         WidthRequest = 75,
                         HorizontalOptions = LayoutOptions.Center,
-                        Content =
-                            new Entry
-                            {                             
-                                Text = $"{c+""+num}",
-                                BackgroundColor = Color.FromRgb(0, 0, 0 ),
-                                HorizontalTextAlignment = TextAlignment.Center
-                            }
-                }
+                        Content = entry
+                    }
                     );
 
                     
                 }
-                num++;
             }
         }
 
 
 
+        /// <summary>
+        ///   This method will be called by the individual Entry elements when Enter
+        ///   is pressed in them.
+        ///   
+        ///   The idea is to move to the next cell in the list.
+        /// </summary>
+        /// <param name="col"> e.g., The 'A' in A5 </param>
+        /// <param name="row"> e.g., The  5  in A5 </param>
+        void handleCellChanged(char col, int row)
+        {
+            Debug.WriteLine($"changed: {col}{row}");
+        }
 
 
         public static void FileMenuNew(object sender, System.EventArgs e)
--- GUI/MyEntry.cs
+++ GUI/MyEntry.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GUI.MainPage;
 
-namespace GUI
-{
-    internal class MyEntry
+namespace GUI;
+
+    /// <summary>
+    ///   Author: H. James de St. Germain
+    ///   Date:   Spring 2023
+    ///   
+    ///   This code shows:
+    ///   1) How to augment a Maui Widget (i.e., Entry) to add more information
+    ///       a) using StyleId
+    ///       b) using fields
+    ///   2) How to provide a method that matches an event handler (used to clear all cells)
+    ///   3) How to attach a method to an event handler (i.e., Completed)
+    /// </summary>
+    public class MyEntry : Entry
     {
+        //JIM: better?--> int row = 0;
+
+        /// <summary>
+        ///   Function provided by "outside world" to be called whenever
+        ///   this entry is modified
+        /// </summary>
+        private ActionOnCompleted onChange;
+
+        private char column;
+        private int row;
+
+        /// <summary>
+        ///   build an Entry element with the row "remembered"
+        /// </summary>
+        /// <param name="row"> unique identifier for this item </param>
+        /// <param name="changeAction"> outside action that should be invoked after this cell is modified </param>
+        public MyEntry(char column, int row, ActionOnCompleted changeAction) : base()
+        {
+
+            this.row = row;
+            this.column = column;
+
+            // Action to take when the user presses enter on this cell
+            this.Completed += CellChangedValue;
+
+            // "remember" outside worlds request about what to do when we change.
+            onChange = changeAction;
+
+            Text = $"{column + "" + row}";
+            BackgroundColor = Color.FromRgb(0, 0, 0);
+            HorizontalTextAlignment = TextAlignment.Center;
+        }
+
+        /// <summary>
+        ///   Remove focus and text from this widget
+        /// </summary>
+        public void ClearAndUnfocus()
+        {
+            this.Unfocus();
+            this.Text = "";
+        }
+
+        /// <summary>
+        ///   Action to take when the value of this entry widget is changed
+        ///   and the Enter Key pressed.
+        /// </summary>
+        /// <param name="sender"> ignored, but should == this </param>
+        /// <param name="e"> ignored </param>
+        private void CellChangedValue(object sender, EventArgs e)
+        {
+            Unfocus();
+
+            // Inform the outside world that we have changed
+            onChange(column, row);
+        }
+
     }
-}
